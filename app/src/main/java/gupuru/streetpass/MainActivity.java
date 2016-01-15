19eca1e14a24509d429b194039f168e745b38228
gupuru <kouhei.aquamarines@gmail.com>
2016-01-15 12:25:51 +0900

    ライブラリ配布できるように修正

--- app/src/main/java/gupuru/streetpass/MainActivity.java
+++ app/src/main/java/gupuru/streetpass/MainActivity.java
@@ -3,18 +3,13 @@ package gupuru.streetpass;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import gupuru.streetpassble.StreetPassBle;
-
-public class MainActivity extends AppCompatActivity implements StreetPassBle.OnStreetPassListener, View.OnClickListener {
-
-    private StreetPassBle streetPassBle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +26,6 @@ public class MainActivity extends AppCompatActivity implements StreetPassBle.OnS
             }
         }
 
-        streetPassBle = new StreetPassBle(MainActivity.this);
-        streetPassBle.setOnStreetPassListener(this);
-
         Button startBtn = (Button) findViewById(R.id.start);
         startBtn.setOnClickListener(this);
         Button stopBtn = (Button) findViewById(R.id.stop);
@@ -41,31 +33,15 @@ public class MainActivity extends AppCompatActivity implements StreetPassBle.OnS
     }
 
     @Override
-    public void streetPassResult(int callbackType, String deviceAddress, String deviceName, String uuid, double distance, String serviceData) {
-        Log.d("ここ", deviceAddress);
-    }
-
-    @Override
-    public void advertiseSuccess(int txPowerLevel, int mode, int timeOut) {
-        Log.d("ここ", "送信成功");
-    }
-
-    @Override
-    public void error(int errorCode, String errorMessage) {
-
-    }
-
-    @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.start:
-                streetPassBle.start("0000180a-0000-1000-8000-00805f9b34fb", "test");
                 break;
             case R.id.stop:
-                streetPassBle.stop();
                 break;
             default:
                 break;
         }
     }
+
 }
