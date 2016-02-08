b1852fb434222aae2d62227c09d8cd3c8f764973
gupuru <kouhei.aquamarines@gmail.com>
2016-02-08 15:42:05 +0900

    初期メッセージのsetterを導入

--- streetpassble/src/main/java/gupuru/streetpassble/parcelable/StreetPassSettings.java
+++ streetpassble/src/main/java/gupuru/streetpassble/parcelable/StreetPassSettings.java
@@ -10,6 +10,7 @@ public class StreetPassSettings implements Parcelable {
     protected String serviceUuid = Settings.SERVICE_UUID;
     protected String characteristicUuid = Settings.CHARACTERISTIC_UUID;
     protected String data = "";
+    protected String defaultResponseData = "";
     protected int scanMode = Settings.SCAN__MODE;
     protected int advertiseMode = Settings.ADVERTISE_MODE;
     protected int txPowerLevel = Settings.TX_POWER_LEVEL;
@@ -28,6 +29,7 @@ public class StreetPassSettings implements Parcelable {
         out.writeString(serviceUuid);
         out.writeString(characteristicUuid);
         out.writeString(data);
+        out.writeString(defaultResponseData);
         out.writeInt(scanMode);
         out.writeInt(advertiseMode);
         out.writeInt(txPowerLevel);
@@ -51,11 +53,15 @@ public class StreetPassSettings implements Parcelable {
     public StreetPassSettings() {
     }
 
-    public StreetPassSettings(String serviceUuid, String characteristicUuid , String data, int scanMode, int advertiseMode, int txPowerLevel
-            , int timeOut, boolean advertiseConnectable, boolean advertiseIncludeDeviceName, boolean advertiseIncludeTxPowerLevel) {
+    public StreetPassSettings(
+            String serviceUuid, String characteristicUuid , String data, String defaultResponseData,
+            int scanMode, int advertiseMode, int txPowerLevel, int timeOut, boolean advertiseConnectable,
+            boolean advertiseIncludeDeviceName, boolean advertiseIncludeTxPowerLevel
+    ) {
         this.serviceUuid = serviceUuid;
         this.characteristicUuid = characteristicUuid;
         this.data = data;
+        this.defaultResponseData = defaultResponseData;
         this.scanMode = scanMode;
         this.advertiseMode = advertiseMode;
         this.txPowerLevel = txPowerLevel;
@@ -69,6 +75,7 @@ public class StreetPassSettings implements Parcelable {
         serviceUuid = in.readString();
         characteristicUuid = in.readString();
         data = in.readString();
+        defaultResponseData = in.readString();
         scanMode = in.readInt();
         advertiseMode = in.readInt();
         txPowerLevel = in.readInt();
@@ -76,6 +83,14 @@ public class StreetPassSettings implements Parcelable {
         advertiseConnectable = in.readByte() != 0;
         advertiseIncludeDeviceName = in.readByte() != 0;
         advertiseIncludeTxPowerLevel = in.readByte() != 0;
+    }
+
+    public String getDefaultResponseData() {
+        return defaultResponseData;
+    }
+
+    public void setDefaultResponseData(String defaultResponseData) {
+        this.defaultResponseData = defaultResponseData;
     }
 
     public String getData() {
